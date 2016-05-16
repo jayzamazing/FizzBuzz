5dd8f1408697c21f4ba724469f63e1980a20e0db
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-05-16 10:24:24 -0400

    Added style.css, added styles for error and h4. Added form and error text to index.html. Modified fizzbuzz to take variable, changed to work off form submit, added preventdefault to stop page reloading, added check to ensure input is a number, convert into int to deal with decimal values.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
-  $('section').html(FizzBuzz());
+  $('#fizzBuzzForm').submit(function(e) {
+    var temp = parseInt($('#fizzBuzzInput').val());
+    if (isNaN(temp)) {
+      $('.errorText').removeClass('error');
+    } else {
+      var max = parseInt(temp);
+      $('.errorText').addClass('error');
+      $('#fizzBuzzOutput').html(FizzBuzz(max));
+    }
+    e.preventDefault();
+  });
 });
-function FizzBuzz(){
+function FizzBuzz(max){
   var text = '';
-  for (i = 1; i <= 100; i++){
+  for (i = 1; i <= max; i++){
     if (i % 3 === 0 && i % 5 === 0){
       text += "fizz buzz" + "<br/>";
     } else if (i % 3 === 0) {
